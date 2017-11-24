dbe75b99460e5b23b3a6f9f0b4bed582694b43a5
Tibin Thomas <tibinthomas@outlook.com>
2017-11-24 17:06:48 +0530

    Improve regex, Fix isssue with nameAlreadyExist check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { PresentsOfSymbols } from './custom.validators';
 import { AbstractControl } from '@angular/forms';
+import * as CustomValidators from './custom.validators';
 
 @Component({
   selector: 'app-root',
@@ -20,47 +20,61 @@ export class AppComponent {
   saveMessageDisplay = false;
 
   // Data Store
-  listOfAllUsers = ['Tibin', 'Thomas'];
+  listOfAllUsers = ['Tibin', 'Tibu', 'Tibi', 'Thibu']; // alwaysKeepInSyncWithsessionStorage
 
 
   constructor(fb: FormBuilder) {
     this.nameOnlyForm = fb.group({
       nameControl: ['', [
-        Validators.minLength(2),
         Validators.required,
         Validators.pattern(/^[A-Z]{1}.*/),
-        PresentsOfSymbols,
-        // this.IsUniqueValidator
+        CustomValidators.atleastTwoAlphaValidator,
+        CustomValidators.presentsOfSymbolsValidator,
+        // CustomValidators.thisNameAlreadyExistValidator
       ]]
-  });
+    });
+
+    sessionStorage.setItem('listOfNamesArray', JSON.stringify(this.listOfAllUsers));
+
+    this.name.valueChanges.subscribe((data) => {         // code should be moved from here
+      this.listOfAllUsers = JSON.parse(sessionStorage.getItem('listOfNamesArray'));
+      if (this.listOfAllUsers.includes(data) ) {
+        this.notUniqueName = true;
+      } else {
+        this.notUniqueName = false;
+      }
+    });
   }
 
   showSaveMessage() {
     this.saveMessageDisplay = true;
-
     setTimeout(() => {
       console.log(this.saveMessageDisplay);
       this.saveMessageDisplay = false;
     }, 5000);
-
   }
 
   onSubmitButtonPress() {
-    this.nameControlValue = this.nameOnlyForm.controls['nameControl'].value;
-    localStorage.setItem(this.nameControlValue, this.nameControlValue);
-    this.listOfAllUsers.push(this.nameControlValue);
-    return this.showSaveMessage();
-
+    const nameJustAdded = this.name.value;
+    this.listOfAllUsers = JSON.parse(sessionStorage.getItem('listOfNamesArray'));
+    this.listOfAllUsers.push(nameJustAdded);
+    sessionStorage.setItem('listOfNamesArray', JSON.stringify(this.listOfAllUsers));
+    // localStorage.removeItem('listOfNamesArray');
+    this.nameOnlyForm.reset();
+    this.showSaveMessage();
   }
 
-  onChangeInText(): boolean {
-    this.nameControlValue = this.nameOnlyForm.controls['nameControl'].value;
-    if (this.listOfAllUsers.includes(this.nameControlValue) ) {
-      return this.notUniqueName = true;
-    } else {
-      return this.notUniqueName = false;
-    }
-  }
+}
+  // onChangeInText(): boolean {
+  //   this.nameControlValue = this.nameOnlyForm.controls['nameControl'].value;
+  //   if (this.listOfAllUsers.includes(this.nameControlValue) ) {
+  //     return this.notUniqueName = true;
+  //   } else {
+  //     return this.notUniqueName = false;
+  //   }
+  // }
+
+
 
   // IsUniqueValidator(control: AbstractControl) {
   //   if (this.notUniqueName) {
@@ -69,7 +83,6 @@ export class AppComponent {
   //       return null;
   // }
 
-}
 
 
 
@@ -78,4 +91,4 @@ export class AppComponent {
 
 
 
-// user = JSON.parse(localStorage.getItem(currentUser));
+// user = JSON.parse(sessionStorage.getItem(currentUser));
--- src/app/custom.validators.ts
+++ src/app/custom.validators.ts
@@ -1,9 +1,14 @@
 import { AbstractControl } from '@angular/forms';
 
-export function PresentsOfSymbols(control: AbstractControl) {
-const regex = new RegExp(/[!%^*()+|~=`{}\[\]:";'<>?,.\/]+/);
-  if (regex.test(control.value)) {
-    return { containsSpecialChars: true };
-  }
-  return null;
+export function presentsOfSymbolsValidator(control: AbstractControl): {[key: string]: any} | null {
+  const regex = new RegExp(/[!%^*()+|~=`{}\[\]:";'<>?,.\/]+/);
+  const regexValidity = regex.test(control.value);
+  return regexValidity ? { 'presentsOfSymbols': { value: control.value }} : null;
 }
+
+export function atleastTwoAlphaValidator(control: AbstractControl): {[key: string]: any} | null {
+  const regex = new RegExp(/^((?!(.*[a-zA-Z].*[a-zA-Z]{1,}.*)).)*$/);
+  const regexValidity = regex.test(control.value);
+  return regexValidity ? { 'atleastTwoAlpha': { value: control.value }} : null;
+}
+
